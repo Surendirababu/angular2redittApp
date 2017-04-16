1a1fea9d8ec45efc45bffbb347630015e6f7447b
surendirababu <babusuren@hotmail.com>
2017-04-16 11:36:12 +0530

    tour of players master details commit

--- reditapp/src/app/app.component.ts
+++ reditapp/src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+  { id: 1, name: 'Murali Vijay' },
+  { id: 2, name: 'K L Rahul' },
+  { id: 3, name: 'Ajinkya Rahane' },
+  { id: 4, name: 'Chetashwar Pujara' },
+  { id: 5, name: 'Virat Kholi' },
+  { id: 6, name: 'Suresh Raina' },
+  { id: 7, name: 'M S Dhoni' },
+  { id: 8, name: 'Ravindra Jadeja' },
+  { id: 9, name: 'Ravi Ashwin' },
+  { id: 10, name: 'Bhuvaneshwar Kumar' },
+  { id: 11, name: 'Ishant Sharma' }
+];
+
 export class Hero{
   id: number;
   name: string;
@@ -9,19 +23,67 @@ export class Hero{
   template: 
   `
   <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>Id: </label>{{hero.id}}</div>
+  <h2>My Players</h2>
+  <ul class="list-group heroes">
+    <li *ngFor = "let hero of heroes" class="list-group-item" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+       {{hero.name}} <span class = "badge">{{hero.id}}</span> 
+    </li>
+  </ul>
+  <div *ngIf = "selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>Id: </label>{{selectedHero.id}}</div>
   <div>
        <label>Name: </label>
-       <input [(ngModel)]="hero.name" placeholder="name">
-  </div>  
+       <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div> 
+  </div>
   `,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #FFB6C1;
+      margin: .5em;
+      padding: .3em 0;
+      height: 2em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+   
+  `]
+
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstormss'
-  };
+  title = 'Tour of Players';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 }
